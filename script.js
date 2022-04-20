5050b812a7daf7c007c309e54ffdfbac91e70719
Robinho1997 <robin.kempen@gmx.de>
2022-04-20 10:53:45 +0200

    Spiel Logik

--- script.js
+++ script.js
@@ -1,55 +1,43 @@
 "use strict";
 
-let antworten = ["Schere","Stein","Papier"]
 
-function computerPlay() {  // wählt zufälligen String aus dem Array
-    return antworten[Math.floor(Math.random() * antworten.length)];
+document.getElementById("Schere").onclick = spieler; 
+document.getElementById("Stein").onclick = spieler;  // ID durch dom abrufen , bei klick soll funktion spieler() ausgeführt werden
+document.getElementById("Papier").onclick = spieler;
 
-}
-
-// führt eine Runde aus
-function playRound(playerSelection,computerSelection) {
-        if (playerSelection == antworten[0] && computerSelection == antworten[2]) {
-        userPunktzahl++; // erhöht die Punktzahl um 1
-        return "Du gewinnst! Schere besiegt Papier.";
-        }
-        else if (playerSelection == antworten[1] && computerSelection == antworten[0]) {
-        userPunktzahl++;
-        return "Du gewinnst! Stein besiegt Schere.";
-        }
-        else if (playerSelection == antworten[2] && computerSelection == antworten[1]) {
-        userPunktzahl++;
-        return "Du gewinnst! Papier besiegt Stein.";
-        }
-        else if (playerSelection == antworten[0] && computerSelection == antworten[1]) {
-        computerPunktzahl++;
-        return "Du verlierst! Stein besiegt Schere."
-        }
-        else if (playerSelection == antworten[1] && computerSelection == antworten[2]) {
-        computerPunktzahl++;
-        return "Du verlierst! Papier besiegt Stein.";
-        }
-        else if (playerSelection == antworten[2] && computerSelection == antworten[0]) {
-        computerPunktzahl++;
-        return "Du verlierst! Schere besiegt Papier.";
-        }
-        else {
-        return "Unentschieden!";
-        }
-}       
-
-let userPunktzahl = parseInt(0);
-let computerPunktzahl = parseInt(0);
+function spieler() {
+    let spielerWahl = this.id;   // Inhalt der Variable spielerWahl soll die ID des Elementes sein das durch klicken die Funktion ausruft
     
-for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Schere, Stein, Papier?");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection,computerSelection));
-    console.log("Deine Punktzahl: " + userPunktzahl);
-    console.log("Computer's Punktzahl: " + computerPunktzahl);
+    let antworten = ["Schere","Stein","Papier"];  // Array mit 3 verschiedenen Werte, jeweils die Antwortmöglichkeiten
+    let computerWahl = antworten[Math.floor(Math.random() * antworten.length)]; // die 3 Werte des Arrays zufällig auswählen und speichern
+
+    document.getElementById("computerAuswahl").innerHTML = "Computerwahl: " + computerWahl;
+    document.getElementById("spielerAuswahl").innerHTML = "Spielerwahl: " + spielerWahl; // Inhalt des Elementes ändern
+    document.getElementById("ergebniss").innerHTML = spielen(spielerWahl,computerWahl); 
+    // Funktion soll ausgeführt werden daraufhin und die Daten danach im Element ausgegeben werden
+
+
+    function spielen(spielerWahl,computerWahl) {  // Funktion mit zwei Parametern, Schere, Stein, Papier. Was soll bei Antworten passieren
+        if (spielerWahl == "Schere" && computerWahl == "Papier") {       
+            return "Du gewinnst!" + "<br>" + "Schere besiegt Papier.";
+            }
+            else if (spielerWahl == "Stein" && computerWahl == "Schere") {  
+            return "Du gewinnst!" + "<br>" + "Stein besiegt Schere.";
+            }
+            else if (spielerWahl == "Papier" && computerWahl == "Stein") {      
+            return "Du gewinnst!" + "<br>" + "Papier besiegt Stein.";
+            }
+            else if (spielerWahl == "Schere" && computerWahl == "Stein") { 
+            return "Du verlierst!" + "<br>" +  "Stein besiegt Schere."
+            }
+            else if (spielerWahl == "Stein" && computerWahl == "Papier") {
+            return "Du verlierst!" + "<br>" +  "Papier besiegt Stein.";
+            }
+            else if (spielerWahl == "Papier" && computerWahl == "Schere") {
+            return "Du verlierst!" + "<br>" +  "Schere besiegt Papier.";
+            }
+            else {
+            return "Unentschieden!";
+            }
+    }       
 }
-
-
- 
-
-
